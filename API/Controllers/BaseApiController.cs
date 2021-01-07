3587d56853d9c7460b7d63b075b58a9beb29b5e4
Nguyen Van Tuyen <36165213+MrTuyen@users.noreply.github.com>
2021-01-07 09:37:14 +0700

    fasdfa
    sdfsdf

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using DS.BusinessObject.API;
+using System.Web.Http;
+
+namespace Invoice.API.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        protected PartnerOAuthBO partnerObject = PartnerOAuthBO.Current.CurrentPartner();
+
+        public BaseApiController()
+        {
+
+        }
+    }
+}
